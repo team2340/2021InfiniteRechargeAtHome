c6e36694a8e83f7b19f39a10231aa76223bf5633
Katie <team2340@gmail.com>
2021-03-27 13:22:31 -0400

    New Subsystems

--- src/main/java/frc/robot/subsystems/ShootingSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShootingSubsystem.java
@@ -1,5 +1,48 @@
 package frc.robot.subsystems;
 
-public class ShootingSubsystem {
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.Robot;
+import frc.robot.RobotMap;
+
+public class ShootingSubsystem extends Subsystem {
+    static private ShootingSubsystem subsystem;
+
+    private ShootingSubsystem() {
+        create();
+	}
+
+    public static ShootingSubsystem getInstance() {
+		if (subsystem == null) {
+			subsystem = new ShootingSubsystem();
+		}
+		return subsystem;
+    }
+
+	private void create() {
+		try {
+			Robot.oi.shooting = new WPI_TalonSRX(RobotMap.SHOOTING_TAL_ID);//change to new talon
+			Robot.oi.shooting.setSensorPhase(true);
+		} catch (Exception ex) {
+			System.out.println("createDumping FAILED");
+		}
+    }
+
     
+    public void shootingForward(double speed){
+        Robot.oi.shooting.set(speed);
+    }
+
+   
+ 
+    public void shootingStop(){
+        Robot.oi.shooting.set(0);
+    }
+
+
+    @Override
+    protected void initDefaultCommand() {
+        //setDefaultCommand(new DumpingCommand());
+    }
 }
--- src/main/java/frc/robot/subsystems/VortexSubsystem.java
+++ src/main/java/frc/robot/subsystems/VortexSubsystem.java
@@ -1,5 +1,48 @@
 package frc.robot.subsystems;
 
-public class VortexSubsystem {
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.Robot;
+import frc.robot.RobotMap;
+
+public class VortexSubsystem extends Subsystem{
+    static private VortexSubsystem subsystem;
+
+    private VortexSubsystem() {
+        create();
+    }
+
+    public static VortexSubsystem getInstance() {
+		if (subsystem == null) {
+			subsystem = new VortexSubsystem();
+		}
+		return subsystem;
+    }
+
+	private void create() {
+		try {
+			Robot.oi.vortex = new WPI_TalonSRX(RobotMap.VORTEX_TAL_ID);//change to new talon
+			Robot.oi.vortex.setSensorPhase(true);
+		} catch (Exception ex) {
+			System.out.println("createVortex FAILED");
+		}
+    }
+
     
+    public void vortexForward(double speed){
+        Robot.oi.vortex.set(speed);
+    }
+
+   
+ 
+    public void vortexStop(){
+        Robot.oi.vortex.set(0);
+    }
+
+
+    @Override
+    protected void initDefaultCommand() {
+        //setDefaultCommand(new VortexCommand());
+    }
 }
