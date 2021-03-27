8fa7b40685fc6793330541b434bf547a8dd3443d
Lindsey <team2340@gmail.com>
2021-03-27 13:23:28 -0400

    New UptakeSubsystem

--- src/main/java/frc/robot/subsystems/UptakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/UptakeSubsystem.java
@@ -1,5 +1,48 @@
 package frc.robot.subsystems;
 
-public class UptakeSubsystem {
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.Robot;
+import frc.robot.RobotMap;
+
+public class UptakeSubsystem extends Subsystem {
+    static private UptakeSubsystem subsystem;
+
+    private UptakeSubsystem() {
+        create();
+	}
+
+    public static UptakeSubsystem getInstance() {
+		if (subsystem == null) {
+			subsystem = new UptakeSubsystem();
+		}
+		return subsystem;
+    }
+
+	private void create() {
+		try {
+			Robot.oi.uptake = new WPI_TalonSRX(RobotMap.UPTAKE_TAL_ID);//change to new talon
+			Robot.oi.uptake.setSensorPhase(true);
+		} catch (Exception ex) {
+			System.out.println("createUptake FAILED");
+		}
+    }
+
     
+    public void uptakeForward(double speed){
+        Robot.oi.uptake.set(speed);
+    }
+
+   
+ 
+    public void uptakeStop(){
+        Robot.oi.uptake.set(0);
+    }
+
+
+    @Override
+    protected void initDefaultCommand() {
+        //setDefaultCommand(new DumpingCommand());
+    }
 }
